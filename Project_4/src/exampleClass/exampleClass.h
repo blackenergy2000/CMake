2f50c50014fb3d4c7fd14087ba097e998bf9cc3c
Enrique Rodriguez <enrique_rodriguez743@aol.com>
2024-05-15 10:54:36 +0200

    Including Project_4 --> Not finished yet

---
+++ Project_4/src/exampleClass/exampleClass.h
@@ -0,0 +1,18 @@
+//
+// Created by kiker on 15.05.2024.
+//
+
+#ifndef EXAMPLECLASS_H
+#define EXAMPLECLASS_H
+
+#include <iostream>
+
+class ExampleClass {
+
+public:
+    ExampleClass() {
+        std::cout << "Class was constructed!";
+    }
+};
+
+#endif //EXAMPLECLASS_H
---
+++ Project_4/src/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by kiker on 15.05.2024.
+//
+
+#include "exampleClass/exampleClass.h"
+
+int main() {
+    ExampleClass test { };
+    return 0;
+}
